dad17cc623710b1f98a4098f27eb024a0ad3da3a
Xujia Zhang <chinalolin321@gmail.com>
2025-06-21 01:52:13 +0000

    [automatic commit] 2025-06-21 01:52:13 UTC 175.35.138.97

--- part1/app.js
+++ part1/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const apiRoutes = require('./routes/api');
+
 var app = express();
 
 app.use(logger('dev'));
