512bfff51e4a287a0206360c4bd0ec8884ac58eb
Xujia Zhang <chinalolin321@gmail.com>
2025-06-21 03:50:34 +0000

    [automatic commit] 2025-06-21 03:50:34 UTC 175.35.138.97

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,22 +37,22 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
+  const [rows] = await db.query(
+    'SELECT * FROM Users WHERE username = ? AND password_hash = ?',
+    [username, password]
+  );
+  if (rows.length === 0) return res.status(401).send('Invalid login');
 
-  try {
-    const [rows] = await db.query(`
-      SELECT user_id, username, role FROM Users
-      WHERE email = ? AND password_hash = ?
-    `, [email, password]);
+  const user = rows[0];
+  req.session.user = user;
 
-    if (rows.length === 0) {
-      return res.status(401).json({ error: 'Invalid credentials' });
-    }
-
-    res.json({ message: 'Login successful', user: rows[0] });
-  } catch (error) {
-    res.status(500).json({ error: 'Login failed' });
+  if (user.role === 'owner') {
+    res.redirect('/owner-dashboard.html');
+  } else {
+    res.redirect('/walker-dashboard.html');
   }
 });
+
 
 module.exports = router;
