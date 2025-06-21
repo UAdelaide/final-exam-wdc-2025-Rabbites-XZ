f5ba499014f6bba2afcd02df88ff5546b02c7455
Xujia Zhang <chinalolin321@gmail.com>
2025-06-21 02:52:31 +0000

    [automatic commit] 2025-06-21 02:52:30 UTC 175.35.138.97

--- part2/app.js
+++ part2/app.js
@@ -7,6 +7,12 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(session({
+  secret: 'dogsecret',
+  resave: false,
+  saveUninitialized: true
+}));
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
